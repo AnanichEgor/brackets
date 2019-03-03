bfdb4bc493195d74e91b4596be671d87060a755b
Egor Ananich <aevbox@gmail.com>
2019-03-03 20:27:53 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 > 0) return false;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      let a = str.split(bracketsConfig[i][0]).length;
+      let b = str.split(bracketsConfig[i][1]).length;
+      if (a != b) {
+          return false;
+      }
+  }
+  return true;
+};
